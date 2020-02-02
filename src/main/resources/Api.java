1753a21926a82bca2a74d53b635652aecf473cfa
Prasoon <prasoon@example.com>
2020-02-02 12:24:58 +0530

    first

---
+++ src/main/resources/Api.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package src.main.resources;
+
+import static org.junit.Assert.*; 
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author prasoon
+ *
+ */
+@Test
+public class Api {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
